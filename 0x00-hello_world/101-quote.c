da0ed01eeaab778896d462b49ae7a00874d642e4
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-07-13 16:22:38 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,13 @@
 /**
  * main - Entry point
  *
- * return 1
+ * Return: 1
  */
 int main(void)
 {
-
-	/* Declare an array to store the message */
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\" - "
+		"Dora Korpar, "
+		"2015-10-19\n";
 
 	/* Excluding the null-terminating character */
 	write(2, message, sizeof(message) - 1);

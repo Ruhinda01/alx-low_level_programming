0eecb67f807239bf3bcb14373fdfb6f8ff0e470a
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-07-14 13:55:27 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int base = 0;
+
+	/* Using the while loop to achieve the result */
+
+	while (base < 10)
+	{
+		printf("%d", base);
+		base++;
+	}
+	printf("\n");
+
+	return (0);
+
+}

3285d7b08c2647585c29669562d5f8ecdb9140e5
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-07-16 03:33:54 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+	int x;
+	int y;
+	int z;
+
+	/* Use for loops and if conditional statements */
+	for (x = '0'; x <= '7'; x++)
+	{
+		for (y = x + 1; y <= '8'; y++)
+		{
+			for (z = y + 1; z <= '9'; z++)
+			{
+				putchar (x);
+				putchar (y);
+				putchar (z);
+				if (x != '7' || y != '8' || z != '9')
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+
+}

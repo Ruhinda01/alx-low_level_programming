94be69635c35dfc49d5c0be51a06f07595968652
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-07-18 16:52:26 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @x: contains first integer
+ * @y: contains second integer
+ * Return: Sum
+ */
+
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}

af96d369b132a7b1b08c7e9c190d06e548d1ec21
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-07-31 13:50:54 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array with 8 elements
+ *
+ * Return: none
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+
+	i = 0;
+	j = 0;
+
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

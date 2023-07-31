f3361f9cff84bb89215a618d740dc3f0f9f2ec1a
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-07-31 10:54:27 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,14 +14,19 @@ unsigned int _strspn(char *s, char *accept)
 
 	length = 0;
 
-	for (i = 0; s[i] != ','; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				length = length + 1;
+				break;
 			}
+		}
+		if (accept[j] == '\0')
+		{
+			break;
 		}
 	}
 	return (length);

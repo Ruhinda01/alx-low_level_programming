e239c7053b7adfe896c5302db3b67b8d792f046e
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-07-31 09:12:10 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * -memcpy - copies memory area
+ *  @dest:  destination
+ *  @src: source
+ *  @n: bytes
+ *
+ *  Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+
+	i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}

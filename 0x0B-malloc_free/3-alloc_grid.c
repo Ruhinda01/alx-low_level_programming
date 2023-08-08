3d35284fc70cd192bfa8e0ba4dee529a9cefc0dd
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-08 06:21:10 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,21 +13,25 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid;
-	int i;
-	int j;
-	int k;
+	int i, j, k;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+
 	grid = (int **)malloc(width * sizeof(int *));
+
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
+
 	for (i = 0; i < width; i++)
 	{
 		grid[i] = (int *)malloc(height * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (k = 0; k < i; k++)
+			for (k = i; k >= 0; k--)
 			{
 				free(grid[k]);
 			}
@@ -35,8 +39,6 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	if (grid == NULL)
-		return (NULL);
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)

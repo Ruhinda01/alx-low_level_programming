64e1d92153a05fcd5baf3862a39fcdf8ba0baa08
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-08 12:09:15 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -17,12 +17,9 @@ char **strtow(char *str)
 	char *s;
 	char **result;
 
-
 	len = 0;
 	if (str == NULL || *str == '\0')
-	{
 		return (NULL);
-	}
 	for (len = 0; str[len] != '\0'; len++)
 	{
 	}
@@ -30,26 +27,18 @@ char **strtow(char *str)
 	s = (char *)malloc(sizeof(char) * len);
 	if (s == NULL)
 		return (NULL);
-
 	result = (char **)malloc(sizeof(char *));
 	if (result == NULL)
-	{
 		free(s);
 		return (NULL);
-	}
-
 	for (i = 0; i < len ; i++)
 	{
 		if (str[i] != ' ')
-		{
 			s[j] = str[i];
 			j++;
-		}
 		else
-		{
 			s[i] = '\n';
 			j++;
-		}
 	}
 	s[j] = '\0';
 	result[0] = s;

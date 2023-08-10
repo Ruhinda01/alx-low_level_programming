fc8b56c51e71fe97ed6d83bdb0e604c0f199c1cb
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-10 14:20:09 -0700

    Correction

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	free(ptr);
 	if (ptr == NULL)
+	{
 		ptr = malloc(new_size);
+		return (ptr);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);

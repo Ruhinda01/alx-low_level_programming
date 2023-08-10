340764c5ba10d1449559e812d204bfaa3a78007b
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-10 09:47:29 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,8 +25,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 
 	}
-	if (len1 == 0 || len2  == 0)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (n >= len2)
 	{
 		total_len = len1 + len2;
@@ -39,13 +41,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (combined == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
-	{
 		combined[i] = s1[i];
-	}
 	for (j = 0; j < n; j++)
-	{
 		combined[i + j] = s2[j];
-	}
 	combined[i + j] = '\0';
 	return (combined);
 }

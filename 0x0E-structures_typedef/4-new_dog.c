db34d23e33ae2fb26452a3694956c979e4709980
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-14 16:09:40 -0700

    A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * string_dup - duplicate a string from source to destination
+ * @src: source file
+ * Return: dest
+ */
+char *string_dup(char *src)
+{
+	int len, i;
+	char *dest;
+
+	if (src == NULL)
+		return (NULL);
+	for (len = 0; src[len] != '\0'; len++)
+	{
+	}
+	dest = malloc((len + 1) * sizeof(char));
+	for (i = 0; i < len + 1; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner name
+ * Return: pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_pup;
+	char *name_dup;
+	char *owner_dup;
+
+	new_pup = malloc(sizeof(dog_t));
+	if (new_pup == NULL)
+		return (NULL);
+	name_dup = string_dup(name);
+	if (name_dup == NULL)
+	{
+		free(new_pup);
+		return (NULL);
+	}
+	owner_dup = string_dup(owner);
+	if (owner_dup == NULL)
+	{
+		free(new_pup);
+		free(name_dup);
+		return (NULL);
+	}
+	(*new_pup).name = name_dup;
+	(*new_pup).age = age;
+	(*new_pup).owner = owner_dup;
+	return (new_pup);
+}

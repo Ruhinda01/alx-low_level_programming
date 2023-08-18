050f18d70d32d3be8929ccfb391e027e07e2bdb0
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-18 07:26:56 -0700

    Fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,53 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+
+/**
+ * print_letter - prints character
+ * @list: va_list
+ * Return: none
+ */
+void print_letter(va_list list)
+{
+	putchar(va_arg(list, int));
+}
+/**
+ * print_integer - prints integer
+ * @list: va_list
+ * Return: none
+ */
+void print_integer(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+/**
+ * print_float - prints float
+ * @list: va_list
+ * Return: none
+ */
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+/**
+ * print_str - prints strings
+ * @list: va_list
+ * Return: none
+ */
+void print_str(va_list list)
+{
+	char *p;
+
+	p = va_arg(list, char *);
+	if (p == NULL)
+	{
+		printf("(nil)");
+	}
+	else if (p != NULL)
+	{
+		printf("%s", p);
+	}
+}
 /**
  * print_all - prints anything
  * @format: list of types of arguments
@@ -11,37 +58,32 @@
 void print_all(const char * const format, ...)
 {
 	va_list list;
-	int i = 0, x;
-	float y;
-	char *p;
+	int i, j;
+	char *s = "";
+
+
+	data arr[] = {
+	{'c', print_letter},
+	{'i', print_integer},
+	{'f', print_float},
+	{'s', print_str},
+	{0, NULL}
+	};
 
 	va_start(list, format);
+	i = 0;
 	while (format[i] != '\0')
 	{
-		if (i > 0 && (format[i] == 'c' || format[i] == 'i'
-			|| format[i] == 'f' || format[i] == 's'))
-			printf(", ");
-		switch (format[i])
+		j = 0;
+		while (arr[j].ty != 0)
 		{
-			case 'c':
-				putchar(va_arg(list, int));
-				break;
-			case 'i':
-				x = va_arg(list, int);
-				printf("%d", x);
-				break;
-			case 'f':
-				y = va_arg(list, double);
-				printf("%f", y);
-				break;
-			case 's':
-				p = va_arg(list, char *);
-				if (p == NULL)
-					printf("(nil)");
-				printf("%s", p);
-				break;
-			default:
-				break;
+			if (arr[j].ty == format[i])
+			{
+				printf("%s", s);
+				arr[j].ptr(list);
+				s = ", ";
+			}
+			j++;
 		}
 		i++;
 	}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include <stdarg.h>
+
 int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
@@ -10,5 +12,25 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
+
+/**
+ * struct type - data format types
+ * @ty: data type characters
+ * @ptr: function pointer to the selected functions
+ */
+
+typedef struct type
+{
+	char ty;
+	void (*ptr)(va_list list);
+} data;
+
+void print_letter(va_list list);
+
+void print_integer(va_list list);
+
+void print_float(va_list list);
+
+void print_str(va_list list);
 
 #endif

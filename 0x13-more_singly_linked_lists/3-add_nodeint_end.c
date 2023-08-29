99246dc04d3ba3f806413767a62fe18edfd65a40
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-29 05:54:39 -0700

    Fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,11 +12,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *last;
 
 	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
 	ptr->n = n;
 	ptr->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = ptr;
+	}
 	else
 	{
 		last = *head;

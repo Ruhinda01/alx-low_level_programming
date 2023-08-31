d72ffc7f5a6abd21addb7fc21697c10e8efed717
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-31 12:47:20 -0700

    Number 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flip_bits - returns the number of bits you need to flip
+ * @n: number
+ * @m number to perform XOR
+ * Return: the number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int num;
+	unsigned int count;
+
+	num = n ^ m;
+	count = 0;
+	while(num)
+	{
+		count = count + (num & 1);
+		num = num >> 1;
+	}
+	return (count);
+}

8f0edcfd79d3b0dc91edf73be9c23f54595b7fde
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-08-31 12:14:20 -0700

    Number 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * clear_bit - sets the value of a bit to 0
+ * @n: number
+ * @index: index starting from 0
+ * Return: 1 if success and -1 if fail
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = ~(1UL << index);
+	if (index < sizeof(unsigned long int) * 8)
+	{
+		*n = *n & mask;
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

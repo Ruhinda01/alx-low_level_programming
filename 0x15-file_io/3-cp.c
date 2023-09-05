97fd60740c0b77ad27ae99060ecf936b2676920d
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-09-05 06:28:33 -0700

    Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <sys/stat.h>
 #define BUF_SIZE 1024
 /**
  * openSrcFile - open source file
@@ -28,6 +29,10 @@ int openDestFile(const char *dest)
 	if (destination == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+		exit(99);
+	}
+	if (fchmod(destination, 0664) == -1)
+	{
 		exit(99);
 	}
 	return (destination);

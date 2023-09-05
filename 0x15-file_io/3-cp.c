cabbc0a67746eaa64a7a68591ab79c79101bfe61
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-09-05 05:30:44 -0700

    Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -73,12 +73,12 @@ void copyFile(int source, int destination, const char *src, const char *dest)
 	}
 	if (close(source) == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", (int)source);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", source);
 		exit(100);
 	}
 	if (close(destination) == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", (int)destination);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", destination);
 		exit(100);
 	}
 }

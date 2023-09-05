9baadd4e98680b4903d0ca9b47cdf6e4315070ce
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-09-05 05:58:39 -0700

    Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	src = argv[1];

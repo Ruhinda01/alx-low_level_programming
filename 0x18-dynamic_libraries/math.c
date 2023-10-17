bee548e05066b55e77ccdeccb359275ffdce0ad3
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-10-17 06:30:35 -0700

    Math

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - adds two integers
+ * @a: number 1
+ * @b: number 2
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtract two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiply two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divide two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
